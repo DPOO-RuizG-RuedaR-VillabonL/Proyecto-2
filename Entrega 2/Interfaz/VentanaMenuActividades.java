a2c741a54269fe70dbd241782e909de8a3144cc7
Felipe34515 <f.rueda4@uniandes.edu.co>
2022-04-30 21:07:48 -0500

    Gestor actividades con error

--- Entrega 2/Interfaz/VentanaMenuActividades.java
+++ Entrega 2/Interfaz/VentanaMenuActividades.java
@@ -1,24 +1,35 @@
 package Interfaz;
+import java.awt.Component;
+
 import javax.swing.JFrame;
 
 
 public class VentanaMenuActividades extends JFrame {
     PAgregaTipoActividad pAgregaTipoActividad;
+    PGestorActividades pGestorActividades;
 
-    public void ventanaMenuActividades () {
+    public void AgregarTipoActividad () {
 
-    pAgregaTipoActividad = new PAgregaTipoActividad(this);
+        pAgregaTipoActividad = new PAgregaTipoActividad(this);
 
-    setTitle("Agregar tipo actividad");
-    setSize(500, 500);
-    setDefaultCloseOperation(EXIT_ON_CLOSE);
-    setLocationRelativeTo(null);
-    setVisible(true);
-    this.add(pAgregaTipoActividad);
+        setTitle("Agregar tipo actividad");
+        setSize(500, 500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        this.add(pAgregaTipoActividad);
     
     }
 
-
-
+    public void GestorActividades () {
+        pGestorActividades = new pGestorActividades(this);
     
+        setTitle("Gestor Actividades");
+        setSize(500, 500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        this.add(pGestorActividades);
+        
+        }
 }
